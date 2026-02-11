c715e8047ea1d247251f736d9830d08bb500ebdf
Bux42 <roszwoka2@gmail.com>
2026-02-11 11:51:18 +0100

    Implement basic cart validation

--- api/src/cart/dto/process-cart.dto.ts
+++ api/src/cart/dto/process-cart.dto.ts
@@ -1,1 +1,29 @@
-export class ProcessCartDto {}
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsDefined,
+  ValidateNested,
+  IsNumber,
+  IsString,
+  IsArray,
+} from 'class-validator';
+
+class CartItemDto {
+  @IsDefined()
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
+
+export class ProcessCartDto {
+  @IsDefined()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CartItemDto)
+  items: CartItemDto[];
+}
