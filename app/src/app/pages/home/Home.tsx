efb8658f96b6854b7b022bdea5dd704f4d26fc0f
Bux42 <roszwoka2@gmail.com>
2026-02-13 18:13:44 +0100

    Create base component

---
+++ app/src/app/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import MovieList from "@/app/components/movies/list/MovieList";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <MovieList />
+    </div>
+  );
+}
